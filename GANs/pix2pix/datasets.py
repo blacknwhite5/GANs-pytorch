95b6594ca8283cf3fec7aa3405a356c2f68df6e8
moohyun <blacknwhite8954@gmail.com>
2019-09-19 17:38:08 +0900

    Add horizontal filp

--- GANs/pix2pix/datasets.py
+++ GANs/pix2pix/datasets.py
@@ -2,12 +2,12 @@ from torch.utils.data import Dataset
 
 import os
 import glob
+import torch
 from PIL import Image
 
 class ImageDataset(Dataset):
     def __init__(self, root, transforms=None, mode='train'):
         self.transform = transforms
-
         self.files = sorted(glob.glob(os.path.join(root, '{}'.format(mode)) + '/*.*'))
 
     def __getitem__(self, index):
@@ -15,8 +15,14 @@ class ImageDataset(Dataset):
         img = self.transform(img)
         h, w = img.shape[1:]
 
-        img_A = img[:,:,:w//2] 
+        img_A = img[:,:,:w//2]
         img_B = img[:,:,w//2:]
+
+        # flip
+        if torch.randn(1) < 0.5:
+            img_A = torch.flip(img_A, dims=(2,))
+            img_B = torch.flip(img_B, dims=(2,))
+            
         return {'A':img_A, 'B':img_B}
 
     def __len__(self):
