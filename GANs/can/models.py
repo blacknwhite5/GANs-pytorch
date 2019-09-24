e236ba786f428720c9a15134a9f2ae2ef2394a06
moohyun <blacknwhite8954@gmail.com>
2019-09-24 16:11:45 +0900

    Add discriminator network

--- GANs/can/models.py
+++ GANs/can/models.py
@@ -31,6 +31,58 @@ class Generator(nn.Module):
         return out 
 
 
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        def block(layer, in_channel, out_channel,
+                  kernel_size=4, stride=2, padding=1, activation='lrelu', norm=True):
+            layers = []
+            # layer
+            if layer == 'conv':
+                layers.append(nn.Conv2d(in_channel, out_channel,
+                                        kernel_size=kernel_size, stride=stride, padding=padding))
+            elif layer == 'linear':
+                layers.append(nn.Linear(in_channel, out_channel))
+            else:
+                raise NotImplementedError('Illegal layer, opts: conv, linear')
+
+            # norm
+            if norm:
+                layers.append(nn.BatchNorm2d(out_channel))
+
+            # activation
+            if activation == 'lrelu':
+                layers.append(nn.LeakyReLU(0.2))
+            elif activation == 'softmax':
+                layers.append(nn.Softmax(dim=0))
+            elif activation == 'sigmoid':
+                layers.append(nn.Sigmoid())
+            else:
+                raise NotImplementedError('Illegal activation, opts: lrelu, softmax, sigmoid')
+            return layers
+
+
+        self.feature = nn.Sequential(*block('conv', 3, 32, norm=False),
+                                     *block('conv', 32, 64),
+                                     *block('conv', 64, 128),
+                                     *block('conv', 128, 256),
+                                     *block('conv', 256, 512),
+                                     *block('conv', 512, 512))
+
+        self.classifier = nn.Sequential(*block('linear', 512*4*4, 1024, norm=False),
+                                        *block('linear', 1024, 512, norm=False),
+                                        *block('linear', 512, 27, norm=False, activation='softmax'))
+
+        self.discriminator = nn.Sequential(*block('linear', 512*4*4, 1, norm=False, activation='sigmoid'))
+
+    def forward(self, x):
+        out = self.feature(x)
+        out = out.view(out.size(0), -1)
+        real_or_fake = self.discriminator(out)
+        cls = self.classifier(out)
+        return cls, real_or_fake
+
 
 if __name__ == '__main__':
     import torch
@@ -39,4 +91,7 @@ if __name__ == '__main__':
     G = Generator()
     fake = G(rand)
 
-    print(fake.shape)
+    D = Discriminator()
+    print(D)
+    cls, rof = D(fake)
+    print(cls.shape, rof.shape)
