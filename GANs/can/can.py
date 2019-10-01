19a0389686b71a2a8b3567b3dcdf5a758f7d48ac
moohyun <blacknwhite8954@gmail.com>
2019-10-01 14:00:47 +0900

    Add weights initialization

--- GANs/can/can.py
+++ GANs/can/can.py
@@ -1,5 +1,5 @@
 import os
-import numpy as np
+import argparse
 
 import torch
 import torch.nn as nn
@@ -9,8 +9,23 @@ from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 import torchvision.transforms as transforms
 
-from models import Discriminator, Generator
+from models import Discriminator, Generator, weights_init
 from datasets import ImageDataset 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--datasets', type=str, default='../../data/wikiart/', help='Directory of datsets')
+parser.add_argument('--num_epoch', type=int, default=100, help='number of episode')
+parser.add_argument('--batch_size', type=int, default=32, help='size of batch')
+parser.add_argument('--latent_space', type=int, default=100, help='size of noise')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate')
+parser.add_argument('--b1', type=float, default=0.5, help='first term of betas')
+parser.add_argument('--b2', type=float, default=0.999, help='second term of betas')
+parser.add_argument('--reuse', action='store_true', help='load pretrained model')
+parser.add_argument('--save_path', type=str, default='pretrained/can.pth', help='the path of saving models')
+parser.add_argument('--num_workers', type=int, default=8, help='number of threads to use for data loading')
+parser.add_argument('--sampling_interval', type=int, default=500, help='Interval of saving results from sampling')
+args = parser.parse_args()
+print(args)
 
 # GPU 사용여부
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -18,14 +33,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # images & pretrained 디렉터리 생성
 os.makedirs('images', exist_ok=True)
 os.makedirs('pretrained', exist_ok=True)
-save_path = 'pretrained/can.pth'
 print('[*]Directories created!')
-
-# 하이퍼 파라매터
-latent_size = 100
-batch_size = 32
-num_epoch = 100
-lr = 0.0002
 
 
 # 이미지 불러오기
@@ -35,15 +43,16 @@ transforms = transforms.Compose([
                 transforms.ToTensor(),
                 transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
 
+
 ### wikiart 데이터 셋
-wikiart = ImageDataset(root='../../data/wikiart/', 
+wikiart = ImageDataset(root=args.datasets, 
                        transforms=transforms) 
 
 ### 데이터 로드
 dataloader = DataLoader(wikiart,
-                        batch_size=batch_size,
+                        batch_size=args.batch_size,
                         shuffle=True,
-                        num_workers=8)
+                        num_workers=args.num_workers)
 
 
 
@@ -52,28 +61,34 @@ def main():
     G = Generator().to(device)
     D = Discriminator().to(device)
 
+    # weights 초기화
+    G.apply(weights_init)
+    D.apply(weights_init)
+    print('[*]weights initialize done')
+
     # pretraind 모델 불러오기
-    if os.path.isfile(save_path):
-        checkpoint = torch.load(save_path)
+    if args.reuse:
+        assert os.path.isfile(args.save_path), '[!]Pretrained model not found'
+        checkpoint = torch.load(args.save_path)
         G.load_state_dict(checkpoint['G'])
         D.load_state_dict(checkpoint['D'])
         print('[*]Pretrained models loaded')
 
     # 최적화
-    D_optim = optim.Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    G_optim = optim.Adam(G.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    D_optim = optim.Adam(D.parameters(), lr=args.lr, betas=(args.b1, args.b2))
+    G_optim = optim.Adam(G.parameters(), lr=args.lr, betas=(args.b1, args.b2))
 
     # loss 함수
     loss_BCE = nn.BCELoss()
     loss_CE = nn.CrossEntropyLoss()
 
-    for epoch in range(num_epoch):
+    for epoch in range(args.num_epoch):
         for i, (img, cls) in enumerate(dataloader):
             img = img.to(device)
             cls = cls.to(device)
 
             # 노이즈 생성
-            z = torch.randn([batch_size,1,1,100]).to(device)
+            z = torch.randn([args.batch_size,1,1,args.latent_space]).to(device)
 
             # # # # #
             # Discriminator
@@ -97,7 +112,7 @@ def main():
             # Generator 
             # # # # #
             loss_G_fake = loss_BCE(D_fake, torch.ones_like(D_fake))
-            loss_G_cls_fake = -((1/27)*torch.ones(batch_size, 27).to(device) \
+            loss_G_cls_fake = -((1/27)*torch.ones(args.batch_size, 27).to(device) \
                                     * nn.LogSoftmax(dim=1)(D_fake_cls)).sum(dim=1).mean()
 
             loss_G = loss_G_fake + loss_G_cls_fake
@@ -108,13 +123,13 @@ def main():
 
 
             # 학습 진행사항 출력
-            print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, num_epoch, i*batch_size, len(wikiart),
+            print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, args.num_epoch, i*args.batch_size, len(wikiart),
                                                                 loss_D.item(), loss_G.item()))
 
 
             # 이미지 저장 (save per epoch)
             batch_done = epoch * len(wikiart) + i
-            if batch_done % 500 == 0:
+            if batch_done % args.sampling_interval == 0:
                 save_image(fake, 'images/{0:03d}.png'.format(batch_done), normalize=True)
 
             # 모델 저장
@@ -122,7 +137,7 @@ def main():
                 'G' : G.state_dict(),
                 'D' : D.state_dict(),
                 },
-                save_path)
+                args.save_path)
 
 if __name__ == '__main__':
     main()
--- GANs/can/models.py
+++ GANs/can/models.py
@@ -84,3 +84,12 @@ class Discriminator(nn.Module):
         real_or_fake = self.discriminator(out)
         cls = self.classifier(out)
         return cls, real_or_fake
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+        m.bias.data.fill_(0)
