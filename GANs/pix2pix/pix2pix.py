795a622a0a603e61cf3da3d95a2290f4a7838b7e
moohyun <blacknwhite8954@gmail.com>
2019-10-01 10:39:55 +0900

    Add argparse

--- GANs/pix2pix/pix2pix.py
+++ GANs/pix2pix/pix2pix.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 import torch
 import torch.nn as nn
@@ -10,20 +11,29 @@ from torchvision.utils import save_image
 from models import UNet, Discriminator, weight_init
 from datasets import ImageDataset
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--datasets', type=str, default='../../data/facades/', help='Directory of datasets')
+parser.add_argument('--num_epoch', type=int, default=200, help='number of episode')
+parser.add_argument('--batch_size', type=int, default=1, help='size of batch')
+parser.add_argument('--lambda_recon', type=int, default=100, help='lambda of reconstruction')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate')
+parser.add_argument('--b1', type=float, default=0.5, help='first term of betas')
+parser.add_argument('--b2', type=float, default=0.999, help='second term of betas')
+parser.add_argument('--reuse', action='store_true', help='load pretrained model')
+parser.add_argument('--save_path', type=str, default='pretrained/pix2pix.pth', help='A name of pretrained model file')
+parser.add_argument('--num_workers', type=str, default=8, help='number of threads to use for data loading')
+args = parser.parse_args()
+print(args)
+
 # GPU 사용여부
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # images & pretrained 디렉터리 생성
 os.makedirs('images', exist_ok=True)
 os.makedirs('pretrained', exist_ok=True)
-save_path = 'pretrained/pix2pix.pth'
-print('Directories created')
+print('[*]Directories created')
 
-
-# 하이퍼 파라매터
-num_epoch = 200
-batch_size = 10
-lambda_recon = 100
 
 # 이미지 불러오기
 ### 이미지 전처리
@@ -33,17 +43,17 @@ transforms = transforms.Compose([
                 transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
 
 ### facades 데이터셋
-datasets = {'{}'.format(mode) : ImageDataset(root='../../data/facades/',
+datasets = {'{}'.format(mode) : ImageDataset(root=args.datasets,
                                 transforms=transforms,
                                 mode=mode)
                                 for mode in ['train', 'test']}
 
 ### 데이터 로드
-dataloader = {'{}'.format(mode) : DataLoader(datasets[mode],
-                                  batch_size=batch_size,
-                                  shuffle=True,
-                                  num_workers=1)
-                                  for mode in ['train', 'test']}
+dataloader = {'{}'.format(mode.split('/')[0]) : DataLoader(datasets[mode.split('/')[0]],
+                                                batch_size=int(mode.split('/')[1]),
+                                                shuffle=True,
+                                                num_workers=args.num_workers)
+                                                for mode in ['train/{}'.format(args.batch_size), 'test/10']}
 
 
 def main():
@@ -56,16 +66,18 @@ def main():
     D.apply(weight_init)
 
     # pretrained 모델 불러오기
-    if os.path.isfile(save_path):
-        checkpoint = torch.load(save_path)
+    if args.reuse:
+        assert os.path.isfile(args.save_path), '[!]Pretrained model not found'
+        checkpoint = torch.load(args.save_path)
         G.load_state_dict(checkpoint['G'])
         D.load_state_dict(checkpoint['D'])
-        print('Pretrained model loaded')
+        print('[*]Pretrained model loaded')
 
-    G_optim = optim.Adam(G.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    D_optim = optim.Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    # optimizer
+    G_optim = optim.Adam(G.parameters(), lr=args.lr, betas=(args.b1, args.b2))
+    D_optim = optim.Adam(D.parameters(), lr=args.lr, betas=(args.b1, args.b2))
 
-    for epoch in range(num_epoch):
+    for epoch in range(args.num_epoch):
         for i, imgs in enumerate(dataloader['train']):
             A = imgs['A'].to(device)
             B = imgs['B'].to(device)
@@ -82,6 +94,7 @@ def main():
 
             # original loss D
             loss_D = -((D_real.log() + (1-D_fake).log()).mean())
+
 #            # LSGAN loss D
 #            loss_D = ((D_real - 1)**2).mean() + (D_fake**2).mean()
     
@@ -99,16 +112,17 @@ def main():
             D_fake = D(fake, B)
 
             # original loss G
-            loss_G = -(D_fake.mean().log()) + lambda_recon * torch.abs(A - fake).mean()
+            loss_G = -(D_fake.mean().log()) + args.lambda_recon * torch.abs(A - fake).mean()
+
 #            # LSGAN loss G
-#            loss_G = ((D_fake-1)**2).mean() + lambda_recon * torch.abs(A - fake).mean()
+#            loss_G = ((D_fake-1)**2).mean() + args.lambda_recon * torch.abs(A - fake).mean()
 
             G_optim.zero_grad()
             loss_G.backward()
             G_optim.step()
 
             # 학습 진행사항 출력
-            print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, num_epoch, i*batch_size, len(datasets['train']),
+            print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, args.num_epoch, i*args.batch_size, len(datasets['train']),
                                                                 loss_D.item(), loss_G.item()))
 
 
@@ -126,7 +140,7 @@ def main():
                 'G' : G.state_dict(),
                 'D' : D.state_dict(),
             },
-            'pretrained/pix2pix.pth')
+            args.save_path)
 
 
 if __name__ == '__main__':
